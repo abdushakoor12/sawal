5cc620c858e48aefec5ee92579bb0fcd3a50503e
abdushakoor12 <abdushakoor009@yahoo.com>
2025-02-16 00:15:50 +0500

    add HTTP-Referer and X-Title for openrouter

--- app/src/main/java/com/abdushakoor12/sawal/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/abdushakoor12/sawal/data/network/RetrofitInstance.kt
@@ -7,10 +7,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     fun getInstance(): Retrofit {
         val client = OkHttpClient.Builder()
-//            .addInterceptor { chain ->
-//                val originalRequest = chain.request()
-//                chain.proceed(chain.request())
-//            }
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val request = original.newBuilder()
+                    .addHeader("HTTP-Referer", "https://github.com/abdushakoor12/sawal")
+                    .addHeader("X-Title", "Sawal")
+                    .build()
+                chain.proceed(request)
+            }
             .build()
         return Retrofit.Builder()
             .baseUrl("https://openrouter.ai/api/v1/")
